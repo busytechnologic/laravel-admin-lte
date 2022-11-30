86889fe2d7cf55d97fbbace227eedcf774675c27
Julio Enrique Duque Vizcaino <68433228+jedv3588@users.noreply.github.com>
2022-11-30 09:13:01 -0600

    Initial commit
    Base project Laravel 8 with AdminLTE

---
+++ resources/views/home.blade.php
@@ -0,0 +1,58 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+        <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Bienvenid@</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+         </div>
+         <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Bienvenid@</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+         </div>
+         <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Bienvenid@</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+         </div>
+         <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Bienvenid@</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+         </div>
+         <div class="card">
+            <div class="card-header">
+                <h1 class="card-title">Bienvenid@</h1>
+            </div>
+            <div class="card-body">
+                <p>Lorem ipsum dolor sit amet</p>
+            </div>
+         </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hola Admin!'); </script>
+@stop
